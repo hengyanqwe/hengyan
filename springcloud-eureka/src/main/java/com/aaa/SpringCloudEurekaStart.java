6abfd6766104f4007add6353392427bd87097198
hengyanqwe <2975216771@qq.com>
2020-08-15 18:32:00 +0800

    提交测试

--- springcloud-eureka/src/main/java/com/aaa/SpringCloudEurekaStart.java
+++ springcloud-eureka/src/main/java/com/aaa/SpringCloudEurekaStart.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-// 启用Eureka服务：将当前应用程序注册成Eureka注册中心(管理服务)
+// 启用Eureka服务：将当前应用程序注册成Eureka注册中心(管理服务) :提交测试
 @EnableEurekaServer
 @SpringBootApplication
 public class SpringCloudEurekaStart {
