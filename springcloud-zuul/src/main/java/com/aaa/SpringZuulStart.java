62ff137e53ed5ce5da198a5eb5937f6a1d33a216
AREA-heart <2840713712@qq.com>
2020-08-21 11:53:29 +0800

    1

--- springcloud-zuul/src/main/java/com/aaa/SpringZuulStart.java
+++ springcloud-zuul/src/main/java/com/aaa/SpringZuulStart.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-// 启用网关代理
+// 启用网关代理 楚程杰
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
